9fc281a28ccd3d924a914237d8539fe980008584
Tira Sundara <sundaralinus@gmail.com>
2025-02-14 07:52:40 +0700

    Split the implementation of TransactionLogger into its own file

--- transaction_logger.go
+++ transaction_logger.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"errors"
-	"fmt"
-	"io"
-	"os"
-)
-
 type TransactionLogger interface {
 	WritePut(key, value string)
 	WriteDelete(key string)
@@ -31,92 +23,4 @@ type Event struct {
 	EventType EventType
 	Key       string
 	Value     string
-}
-
-type FileTransactionLogger struct {
-	events       chan<- Event // Writeonly channel for sending events
-	errors       <-chan error // read-only channel for receiving errors
-	lastSequence uint64       // the last used event sequence number
-	file         *os.File     // the location of the transaction log
-}
-
-func NewFileTranscationLogger(filename string) (TransactionLogger, error) {
-	file, err := os.OpenFile(filename, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0755)
-	if err != nil {
-		return nil, fmt.Errorf("cannot open transaction log file: %w", err)
-	}
-
-	return &FileTransactionLogger{file: file}, nil
-}
-
-func (l *FileTransactionLogger) WritePut(key, value string) {
-	l.events <- Event{EventType: EventPut, Key: key, Value: value}
-}
-
-func (l *FileTransactionLogger) WriteDelete(key string) {
-	l.events <- Event{EventType: EventDelete, Key: key}
-}
-
-func (l *FileTransactionLogger) Err() <-chan error {
-	return l.errors
-}
-
-func (l *FileTransactionLogger) Run() {
-	events := make(chan Event, 16) // Make an events channel
-	l.events = events
-
-	errors := make(chan error, 1) // Make an errors channel
-	l.errors = errors
-
-	go func() {
-		for e := range events { // Retrieve the next Event
-
-			l.lastSequence++ // Increment sequence number
-
-			_, err := fmt.Fprintf(l.file, "%d\t%d\t%s\t%s\n", l.lastSequence, e.EventType, e.Key, e.Value) // Write the event to the log
-			if err != nil {
-				errors <- err
-				return
-			}
-		}
-	}()
-}
-
-func (l *FileTransactionLogger) ReadEvents() (<-chan Event, <-chan error) {
-	scanner := bufio.NewScanner(l.file) // Create a Scanner for l.file
-	outEvent := make(chan Event)        // An unbuffered Event channel
-	outError := make(chan error, 1)     // A buffered error channel
-
-	go func() {
-		var e Event
-
-		defer close(outEvent) // Close the channels when the
-		defer close(outError) // goroutine ends
-
-		for scanner.Scan() {
-			line := scanner.Text()
-
-			if _, err := fmt.Sscanf(line, "%d\t%d\t%s\t%s", &e.Sequence, &e.EventType, &e.Key, &e.Value); err != nil && !errors.Is(err, io.EOF) {
-				outError <- fmt.Errorf("input parse error: %w", err)
-				return
-			}
-
-			// Sanity check! are the sequence numbers in increasing order?
-			if l.lastSequence >= e.Sequence {
-				outError <- fmt.Errorf("transaction numbers out of sequence")
-				return
-			}
-
-			l.lastSequence = e.Sequence // Update last used sequence #
-
-			outEvent <- e // Send the event along
-		}
-
-		if err := scanner.Err(); err != nil {
-			outError <- fmt.Errorf("transaction log read failure: %w", err)
-			return
-		}
-	}()
-
-	return outEvent, outError
 }
